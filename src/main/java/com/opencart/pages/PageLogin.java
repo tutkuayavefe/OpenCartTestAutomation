22cef0c215d8fcb648d50b297ca1e7f18ece360f
tutkuayavefe <tutkuayavefe@gmail.com>
2021-02-07 20:52:49 +0100

    login tests added

--- src/main/java/com/opencart/pages/PageLogin.java
+++ src/main/java/com/opencart/pages/PageLogin.java
@@ -7,10 +7,17 @@ public class PageLogin {
 	private String email = "//input[@id='input-email']";
 	private String password = "//input[@id='input-password']";
 	private String loginButton = "//button[@class='btn btn-primary btn-lg hidden-xs']";
+	private String pinInput = "input-pin";
+	private String continueButton = "/html/body/div/div[2]/div/div[1]/form/div[2]/button";
 	private String welcomeText = "//p[contains(.,'Welcome to OpenCart!')]";
+	private String alertMessage = "//div[@class='alert alert-danger']";
+	private String forgetPasswordButton = "//a[@href='https://www.opencart.com/index.php?route=account/forgotten']";
+	private String forgetSubmit = "//button[contains(.,'Submit')]";
+	private String alertSuccess = "//div[@class='alert alert-success']";
 
 	private String userEmail = "tutkuayavefe@mailinator.com";
 	private String userPassword = "Mailinator12.";
+	private String userPin = "1357";
 
 	public PageLogin(WebDriver driver) {
 		// TODO Auto-generated constructor stub
@@ -41,4 +48,31 @@ public class PageLogin {
 		return userPassword;
 	}
 
+	public String getPinInput() {
+		return pinInput;
+	}
+
+	public String getContinueButton() {
+		return continueButton;
+	}
+
+	public String getUserPin() {
+		return userPin;
+	}
+
+	public String getAlertMessage() {
+		return alertMessage;
+	}
+
+	public String getForgetPasswordButton() {
+		return forgetPasswordButton;
+	}
+
+	public String getForgetSubmit() {
+		return forgetSubmit;
+	}
+
+	public String getAlertSuccess() {
+		return alertSuccess;
+	}
 }
--- src/test/java/com/opencart/tests/PageLoginTests.java
+++ src/test/java/com/opencart/tests/PageLoginTests.java
@@ -1,5 +1,13 @@
 package com.opencart.tests;
 
+import java.awt.print.Pageable;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.opencart.base.BaseTest;
@@ -8,15 +16,96 @@ import com.opencart.pages.PageRegister;
 
 public class PageLoginTests extends BaseTest {
 	PageLogin pageLogin;
-
+	
+	 
 	@Test(priority = 1)
-	public void LoginToOpenCart() {
+	public void FillAndLoginToOpenCart() {
 		// This test is to verify that
 		pageLogin = new PageLogin(driver);
 		driver.get("https://www.opencart.com/index.php?route=account/login");
 
+		WebElement email = driver.findElement(By.xpath(pageLogin.getEmail()));
+		if (email.isDisplayed()) {
+			email.click();
+			email.sendKeys(pageLogin.getUserEmail());
+		}
+
+		WebElement password = driver.findElement(By.xpath(pageLogin.getPassword()));
+		if (password.isDisplayed()) {
+			password.click();
+			password.sendKeys(pageLogin.getUserPassword());
+		}
+
+		WebElement loginButton = driver.findElement(By.xpath(pageLogin.getLoginButton()));
+		if (loginButton.isDisplayed()) {
+			loginButton.click();
+		}
 		
+		
+		WebElement pin = driver.findElement(By.id(pageLogin.getPinInput()));
+		if(pin.isDisplayed()) {
+			pin.click();
+			pin.sendKeys(pageLogin.getUserPin());
+		}
+		
+		
+		WebElement continueButton = driver.findElement(By.xpath(pageLogin.getContinueButton()));
+		if(continueButton.isDisplayed()) {
+			continueButton.click();
+		}
 		
 
+		WebElement welcomeText = driver.findElement(By.xpath(pageLogin.getWelcomeText()));
+		Assert.assertEquals("Welcome to OpenCart!", welcomeText.getText(), "They are not equal: "+ welcomeText.getText());
 	}
+	
+	@Test(priority = 2)
+	public void invalidLoginCridentials(){
+		pageLogin = new PageLogin(driver);
+		driver.get("https://www.opencart.com/index.php?route=account/login");
+		 
+		
+		WebElement loginButton = driver.findElement(By.xpath(pageLogin.getLoginButton()));
+		if (loginButton.isDisplayed()) {
+			loginButton.click();
+		}
+		
+		WebElement alertMessage = driver.findElement(By.xpath(pageLogin.getAlertMessage()));
+		
+		if(alertMessage.isDisplayed()) {
+			System.out.println("Test passed!");
+		}
+	}
+	
+	
+	@Test(priority = 3)
+	public void forgetPassword() {
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		pageLogin = new PageLogin(driver);
+		driver.get("https://www.opencart.com/index.php?route=account/login");
+	
+		WebElement forgetPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pageLogin.getForgetPasswordButton())));
+		if (forgetPassword.isDisplayed()) {
+			forgetPassword.click();
+		}
+		
+		WebElement email = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pageLogin.getEmail())));
+	
+			email.click();
+			email.sendKeys(pageLogin.getUserEmail());
+		
+		
+		WebElement forgetSubmit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pageLogin.getForgetSubmit())));
+		if (forgetSubmit.isDisplayed()) {
+			forgetSubmit.click();
+		}
+		
+		WebElement alertMessageVisibility=	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pageLogin.getAlertSuccess())));
+		
+	
+	}
+	
+	
 }
