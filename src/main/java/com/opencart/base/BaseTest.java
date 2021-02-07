13d56dcf697df5276d67bc19b3f44efdb74a4570
tutkuayavefe <tutkuayavefe@gmail.com>
2021-02-07 16:07:01 +0100

    page register tests and page methods have been added

--- src/main/java/com/opencart/base/BaseTest.java
+++ src/main/java/com/opencart/base/BaseTest.java
@@ -1,5 +1,25 @@
 package com.opencart.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+	public WebDriver driver;
+
+	@BeforeMethod
+	protected void methodSetup() {
+		System.out.println("Method set up ");
+		System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+	}
+
+	@AfterMethod
+	protected void methodTearDown() {
+		System.out.println("Method tear down");
+		// driver.quit();
+	}
 
 }
