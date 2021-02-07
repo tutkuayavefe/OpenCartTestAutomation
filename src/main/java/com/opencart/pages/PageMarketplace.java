c98513e68a73a1e86855d1682b02b6e1c28cdd14
tutkuayavefe <tutkuayavefe@gmail.com>
2021-02-07 22:18:38 +0100

    Page Marketplace tests added

--- src/main/java/com/opencart/pages/PageMarketplace.java
+++ src/main/java/com/opencart/pages/PageMarketplace.java
@@ -3,12 +3,88 @@ package com.opencart.pages;
 import org.openqa.selenium.WebDriver;
 
 public class PageMarketplace {
+
+	private String freeButton = "(//a[contains(.,'Free')])[1]";
+	private String facebookFree = "(//div[@class='extension-description'])[1]";
+	private String firstDownloadButton = "//a[@class='btn btn-success btn-lg btn-block']";
+	private String secondDownloadButton = "//a[@class='btn btn-primary']";
+	private String searchField = "//input[contains(@name,'search')]";
+	private String relevantResult = "(//a[contains(.,'Language Editor')])[2]";
+	private String freeProductText = "(//span[contains(.,'FREE')])[1]";
+	private String sortField = "//select[@id='input-sort']";
+
 	
-	
-	
-	
+
+	private String email = "//input[@id='input-email']";
+	private String password = "//input[@id='input-password']";
+	private String userEmail = "tutkuayavefe@mailinator.com";
+	private String userPassword = "Mailinator12.";
+	private String loginButton = "//button[@class='btn btn-primary btn-lg hidden-xs']";
+	private String pinInput = "input-pin";
+	private String continueButton = "/html/body/div/div[2]/div/div[1]/form/div[2]/button";
+
 	public PageMarketplace(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 
+	}
+
+	public String getFreeButton() {
+		return freeButton;
+	}
+
+	public String getFacebookFree() {
+		return facebookFree;
+	}
+
+	public String getFirstDownloadButton() {
+		return firstDownloadButton;
+	}
+
+	public String getSecondDownloadButton() {
+		return secondDownloadButton;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public String getLoginButton() {
+		return loginButton;
+	}
+
+	public String getPinInput() {
+		return pinInput;
+	}
+
+	public String getContinueButton() {
+		return continueButton;
+	}
+
+	public String getSearchField() {
+		return searchField;
+	}
+
+	public String getRelevantResult() {
+		return relevantResult;
+	}
+	
+	public String getFreeProductText() {
+		return freeProductText;
+	}
+
+	public String getSortField() {
+		return sortField;
 	}
 }
--- src/test/java/com/opencart/tests/PageMarketplaceTests.java
+++ src/test/java/com/opencart/tests/PageMarketplaceTests.java
@@ -1,8 +1,116 @@
 package com.opencart.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import com.opencart.base.BaseTest;
+import com.opencart.pages.PageLogin;
 import com.opencart.pages.PageMarketplace;
 
 public class PageMarketplaceTests extends BaseTest {
 	PageMarketplace pageMarketPlace;
+	
+	public void LoginProcess() {
+		driver.get("https://www.opencart.com/index.php?route=account/login");
+		WebElement email = driver.findElement(By.xpath(pageMarketPlace.getEmail()));
+		if (email.isDisplayed()) {
+			email.click();
+			email.sendKeys(pageMarketPlace.getUserEmail());
+		}
+
+		WebElement password = driver.findElement(By.xpath(pageMarketPlace.getPassword()));
+		if (password.isDisplayed()) {
+			password.click();
+			password.sendKeys(pageMarketPlace.getUserPassword());
+		}
+
+		WebElement loginButton = driver.findElement(By.xpath(pageMarketPlace.getLoginButton()));
+		if (loginButton.isDisplayed()) {
+			loginButton.click();
+		}
+		
+		WebElement pin = driver.findElement(By.id(pageMarketPlace.getPinInput()));
+		if(pin.isDisplayed()) {
+			pin.click();
+			pin.sendKeys("1357");
+		}
+		
+		
+		WebElement continueButton = driver.findElement(By.xpath(pageMarketPlace.getContinueButton()));
+		if(continueButton.isDisplayed()) {
+			continueButton.click();
+			
+		driver.get("https://www.opencart.com/index.php?route=marketplace/extension");
+
+		}
+	}
+	
+	@Test(priority = 1)
+	public void DownloadFirstFreeProduct() {
+		// This test is to verify that
+		pageMarketPlace = new PageMarketplace(driver);
+
+		LoginProcess();
+		
+		WebElement freeButton = driver.findElement(By.xpath(pageMarketPlace.getFreeButton()));
+		freeButton.click();
+		
+
+		WebElement freeProduct = driver.findElement(By.xpath(pageMarketPlace.getFacebookFree()));
+		freeProduct.click();
+		
+		
+		WebElement firstDownloadButton = driver.findElement(By.xpath(pageMarketPlace.getFirstDownloadButton()));
+		firstDownloadButton.click();
+		
+
+		WebElement secondDownloadButton = driver.findElement(By.xpath(pageMarketPlace.getSecondDownloadButton()));
+		secondDownloadButton.click();
+	}
+	
+	
+	@Test(priority = 2)
+	public void TestSearch() throws InterruptedException {
+		// This test is to verify that
+		pageMarketPlace = new PageMarketplace(driver);
+
+		LoginProcess();
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		WebElement searchField = driver.findElement(By.xpath(pageMarketPlace.getSearchField()));
+		searchField.click();
+		searchField.sendKeys("Language");
+		searchField.sendKeys(Keys.ENTER);
+		
+
+		
+		WebElement relevantElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pageMarketPlace.getRelevantResult())));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(200,200)");
+		relevantElement.getText().contains("language");
+	
+	}
+	
+	
+	@Test(priority = 3)
+		public void SortByPriceFreeFirst() {
+		pageMarketPlace = new PageMarketplace(driver);
+		LoginProcess();
+
+		WebElement sort = driver.findElement(By.xpath(pageMarketPlace.getSortField()));
+		sort.click();
+		
+		for(int i = 0; i<4;i++) {
+			sort.sendKeys(Keys.DOWN);
+		}
+		
+		
+		}
+	
 }
